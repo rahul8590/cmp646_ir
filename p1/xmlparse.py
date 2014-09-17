99ba1591fb8510371b15cd82ef474c69527de2c9
rahul8590@gmail.com <rahul8590@gmail.com>
2014-09-16 20:19:25 -0400

    populate dict of words with sanitization function

--- p1/xmlparse.py
+++ p1/xmlparse.py
@@ -3,20 +3,32 @@ The file is response for parsing xml and compiling statistics
 """
 
 #!/bin/python
-
 import xml.etree.cElementTree as cxml
-
+import re
 
 
 """
-dict_word['word'] = [[(book_name,page_no),
+dict_word['word'] = [[(book_name,[page_nos]),
                       (book_name,page_no)],
                       count]
 """
 
+dword = {}
 
-def parse_line(line):
-  #Code to parse line returned from parse_xml 
+def populate_dict(word):
+  dword[word] = d.get(w,0) + 1 
+
+
+
+#line is a simple string which can get junk characters
+def sanitize_line(line):
+   rx = re.compile('\W+') #Getting only alphanumeric letters first
+   #rnos = re.compile('[0-9]')
+   rline = rx.sub(' ',line).strip()
+   #nline = rnos.subl(' ',rline).strip() #Only words and possible extra whitespaces. 
+   #re.sub(' +', ' ',nline)
+   return rline #string 
+
 
 
 def parse_xml(filename):
@@ -24,4 +36,7 @@ def parse_xml(filename):
     if elem.tag == 'page':
       print elem.attrib
       for node in elem.findall('./region/section/line'):
-        print node.text
+        # node.text  will have unsanitized <line> information.  
+        san_string = sanitize_line(node.text)
+        for i in san_string.split(' '): #populating the dictionary of words
+          populate_dict(i)
