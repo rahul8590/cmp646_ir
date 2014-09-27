4fff466b11c2f2016b6782d90b9b06e2eb01f96e
rahul8590@gmail.com <rahul8590@gmail.com>
2014-09-27 17:22:05 -0400

    fixed string parsing, getting better nos now.

--- p1/xmlparse_backup.py
+++ p1/xmlparse_backup.py
@@ -12,13 +12,9 @@ import multiprocessing as mp
 #import itertools 
 import collections 
 import cPickle as pickle
-import tokenize , token
-
 
 from pympler import summary
 from pympler import muppy
-
-
 
 """
 dict_word['word'] = [[(book_name,[page_nos]),
@@ -27,7 +23,7 @@ dict_word['word'] = [[(book_name,[page_nos]),
 """
 
 #All the global variables will be declared in here 
-#dword = {}
+dword = {}
 itvalue = 0
 #-------------------------------------------------
 
@@ -49,12 +45,12 @@ def populate_dict(word):
 def sanitize_line(line_n):
    if line_n == None or line_n == ' ': return None
    line1 = str(line_n)
-   #line1 = line1.lower()
+   line1 = line1.lower()
    #print "the line is ", line1
-   #rx = re.compile('\W+') #Getting only alphanumeric letters first
-   #line_ascii = re.sub(r'[^\x00-\x7F]+',' ', line1)  #Remove non-ascii elements from string
+   rx = re.compile('\W+') #Getting only alphanumeric letters first
+   line_ascii = re.sub(r'[^\x00-\x7F]+',' ', line1)  #Remove non-ascii elements from string
    #rnos = re.compile('[0-9]')
-   rline = re.sub('\W+',' ',line1).strip().lower()
+   rline = rx.sub(' ',line_ascii).strip()
    #nline = rnos.subl(' ',rline).strip() #Only words and possible extra whitespaces. 
    #re.sub(' +', ' ',nline)
    return rline #string 
@@ -69,22 +65,11 @@ def parse_xml(filename):
       if elem.tag == 'page':
         #print elem.attrib
         for node in elem.findall('./region/section/line'):
-          # node.text  will have unsanitized <line> information.
-          for line in node.itertext():
-            #commenting the regular procedure to sanitize string
-            #print "node to be sanitizes =>" , node.text
-            san_string = sanitize_line(line)
-            #print "after sanitization =>", san_string
-            if san_string == None or san_string == ' ': continue
-            else:
-              for i in san_string.split(' '): #populating the dictionary of words
-                dword[i] = dword.get(i,0) + 1
-          """
-          for t in tokenize.generate_tokens(iter([node.text]).next):
-            if token.tok_name[t[0]] == 'STRING':
-              dword[t[1]] = dword.get(t[1],0) + 1
-              print t[1]
-          """
+          # node.text  will have unsanitized <line> information.  
+          san_string = sanitize_line(node.text)
+          if san_string == None or san_string == ' ': continue
+          for i in san_string.split(' '): #populating the dictionary of words
+            dword[i] = dword.get(i,0) + 1
   except:
     print filename , "is having Encoding problems or parsing errors"
   #all_objects = muppy.get_objects()
@@ -101,22 +86,16 @@ if __name__ == '__main__':
   pool = mp.Pool(processes=numthreads)
   dword_list = pool.map(parse_xml, (locate("*.xml")))
   #final_dword = bsddb.btopen('wordc','c')
-  final_dword = collections.Counter()
+  #final_dword = collections.Counter()
   #map(final_dword.update,dword_list)
   #final_dword = dict(kv for d in dictlist for kv in d.iteritems())
   for i in dword_list:
     final_dword.update(i)
-  """
   print "dumping dword_list into pickle format"
   with open('word_count_big_list.pickle', 'wb') as handle:
     pickle.fast = True 
     pickle.dump(dword_list, handle)
-  """
-  #print final_dword
-  print "The final Word Count List is calculated ", len(final_dword)
-  print "total no of tokens" , sum(final_dword.values())
-  print "most common words are ", final_dword.most_common(50)
-  print "no => " , final_dword['no']
+  print "The final Word Count List is calculated and dumped"
 
 
 
